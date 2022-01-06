1a9431639e3c16872950a678807ad6de7146862d
Vito <vito.arcidiacono@gmail.com>
2022-01-06 02:04:19 +0100

    retrieve dei prodotti da db

--- src/Controller/CatalogoController.php
+++ src/Controller/CatalogoController.php
@@ -5,12 +5,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Prodotti;
 use App\Repository\ProdottiRepository;
+use App\classes\ProdottoFactory;
 
 class CatalogoController extends AbstractController
 {
     public function getCatalogo(ProdottiRepository $prodottiRepository): Response
     {
         $prodotti = $prodottiRepository->findAll();
+       
+        foreach($prodotti as $i => $prodotto ){
+            $prodotti[$i] = ProdottoFactory::getProdotto($prodotto);
+        }
         
         if (!$prodotti) {
             throw $this->createNotFoundException(
@@ -19,7 +24,7 @@ class CatalogoController extends AbstractController
         }
         
         return $this->render('catalogo.html2.twig', [
-            'prodotti' => $prodotti,
+            'prodotti' => array_chunk($prodotti, 3)
         ]);
         
     }
